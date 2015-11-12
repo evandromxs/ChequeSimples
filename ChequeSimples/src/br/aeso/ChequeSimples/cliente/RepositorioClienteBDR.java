6d25e144344317acbaa0c756aa1ff2656be9507e
Evandro <Evandro@EvandroPC.home>
2015-11-11 21:43:30 -0300

    Repositório de Cliente pronto

--- ChequeSimples/src/br/aeso/ChequeSimples/cliente/RepositorioClienteBDR.java
+++ ChequeSimples/src/br/aeso/ChequeSimples/cliente/RepositorioClienteBDR.java
@@ -14,47 +14,49 @@ import br.aeso.ChequeSimples.excecoes.ClientesVazioException;
 
 public class RepositorioClienteBDR implements IRepositorioCliente {
 	private Connection conexao;
-	
+
 	public RepositorioClienteBDR() {
-    	try {
+		try {
 			conexao = DriverManager.getConnection("jdbc:mysql://localhost/chequesimplesbdr", "root", "root");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	@Override
 	public void cadastrar(Cliente cliente) throws ClienteJaCadastradoException {
 		// Verifica se o cliente já existe no repositório
-		if (!this.existe(cliente.getCpf_cnpj())) throw new ClienteJaCadastradoException();
-        // Criando a String SQL
-        String sql ="insert into cliente (pessoa, cpf_cnpj, nome_razaoSocial, bloqueado) values (?, ?, ?, ?)";
- 
-        // Criar o PreparedStatement, objeto para executar a query
-        PreparedStatement preStatement = null;
-        ResultSet resultSet = null;
+		if (!this.existe(cliente.getCpf_cnpj()))
+			throw new ClienteJaCadastradoException();
+		// Criando a String SQL
+		String sql = "insert into cliente (pessoa, cpf_cnpj, nome_razaoSocial, bloqueado) values (?, ?, ?, ?)";
+
+		// Criar o PreparedStatement, objeto para executar a query
+		PreparedStatement preStatement = null;
+		ResultSet resultSet = null;
 		try {
 			preStatement = conexao.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		
-	        // Atualizando o primeiro parametro
-	        preStatement.setString(1, cliente.getPessoa());
-	        preStatement.setString(2, cliente.getCpf_cnpj());
-	        preStatement.setString(3, cliente.getNome_razaoSocial());
-	        preStatement.setInt(4, cliente.getBloqueado());
-	        preStatement.execute();
-	        // Retorna um ResultSet com todas as chaves geradas
-	        resultSet = preStatement.getGeneratedKeys();
-	        Integer clienteId = 0;
-	        // Pegando o identificador gerado a partir do último insert
-	        while(resultSet.next()) {
-	        	clienteId = resultSet.getInt(1);
-	        }
-	        cliente.setId(clienteId);
-	        
-	        // Fechando conexões
-	        preStatement.close();
-	        resultSet.close();
-	        
+
+			// Atualizando o primeiro parametro
+			preStatement.setString(1, cliente.getPessoa());
+			preStatement.setString(2, cliente.getCpf_cnpj());
+			preStatement.setString(3, cliente.getNome_razaoSocial());
+			preStatement.setInt(4, cliente.getBloqueado());
+			preStatement.execute();
+			// Retorna um ResultSet com todas as chaves geradas
+			resultSet = preStatement.getGeneratedKeys();
+			Integer clienteId = 0;
+			// Pegando o identificador gerado a partir do último insert
+			while (resultSet.next()) {
+				clienteId = resultSet.getInt(1);
+			}
+			cliente.setId(clienteId);
+
+			// Fechando conexões
+			preStatement.close();
+			resultSet.close();
+			conexao.close();
+
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
@@ -62,80 +64,175 @@ public class RepositorioClienteBDR implements IRepositorioCliente {
 
 	@Override
 	public void atualizar(Cliente cliente) throws ClienteNaoEncontradoException {
-        int i = getIndice(cliente.getCpf_cnpj());
-        if (i == -1) throw new ClienteNaoEncontradoException();
-       // clientes.set(i, cliente);
+		int i = getIndice(cliente.getCpf_cnpj());
+		if (i == -1){
+			throw new ClienteNaoEncontradoException();
+		} else {
+			try {
+				String sql = "update cliente set pessoa = ?, cpf_cnpj = ?, nome_razaoSocial = ?, bloqueado = ? where idCliente = ?";
+				PreparedStatement preStatement = conexao.prepareStatement(sql);
+				
+				preStatement.setString(1, cliente.getPessoa());
+				preStatement.setString(2, cliente.getCpf_cnpj());
+				preStatement.setString(3, cliente.getNome_razaoSocial());
+				preStatement.setByte(4, cliente.getBloqueado());
+				preStatement.setInt(5, i);
+				
+				preStatement.executeUpdate();
+
+				// Fechando conexões
+				preStatement.close();
+				conexao.close();
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
 	@Override
-	public void remover(String cpf) throws ClienteNaoEncontradoException {
-		 int i = getIndice(cpf);
-	        if (i == -1) throw new ClienteNaoEncontradoException();
-	       //clientes.remove(i);
+	public void remover(String cpf_cnpj) throws ClienteNaoEncontradoException {
+		int i = getIndice(cpf_cnpj);
+		if (i == -1) {
+			throw new ClienteNaoEncontradoException();
+		} else {
+			try {
+				String sql = "delete from clientes where cliente_id = ?";
+				PreparedStatement preStatement = conexao.prepareStatement(sql);
+				preStatement.setInt(1, i);
+				preStatement.executeUpdate();
+
+				// Fechando conexões
+				preStatement.close();
+				conexao.close();
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
 	@Override
 	public ArrayList<Cliente> listar() throws ClientesVazioException {
-		if(listaVazia() == true){
+		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+		if (listaVazia() == true) {
 			throw new ClientesVazioException();
-		}else{
-			//return clientes;
-			return null;
+		} else {
+			try {
+				String sql = "select * from clientes";
+				PreparedStatement preStatement = conexao.prepareStatement(sql);
+				ResultSet resultSet = preStatement.executeQuery();
+
+				while (resultSet.next()) {
+					Cliente cliente = new Cliente();
+					cliente.setId(resultSet.getInt(1));
+					cliente.setPessoa(resultSet.getString(2));
+					cliente.setCpf_cnpj(resultSet.getString(3));
+					cliente.setPessoa(resultSet.getString(4));
+					cliente.setBloqueado(resultSet.getByte(5));
+
+					clientes.add(cliente);
+				}
+
+				// Fechando conexões
+				preStatement.close();
+				resultSet.close();
+				conexao.close();
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+
+			return clientes;
 		}
-	}
-	
-	@Override
-	public Cliente procurar(String cpf) throws ClienteNaoEncontradoException {
-        int i = getIndice(cpf);
-        if (i == -1) throw new ClienteNaoEncontradoException();
-        
-        //return clientes.get(i);
-        return null;
 	}
 
 	@Override
-	public boolean existe(String cpf) {
-	boolean resposta;
-          if (getIndice(cpf) >= 0) resposta = true;
-          else resposta = false;
-      return resposta;
+	public Cliente procurar(String cpf_cnpj) throws ClienteNaoEncontradoException {
+		Cliente cliente = new Cliente();
+		int i = getIndice(cpf_cnpj);
+		if (i == -1) {
+			throw new ClienteNaoEncontradoException();
+		} else {
+			try {
+				String sql = "select * from clientes where cpf_cnpj = ?";
+				PreparedStatement preStatement = conexao.prepareStatement(sql);
+				preStatement.setString(1, cpf_cnpj);
+				ResultSet resultSet = preStatement.executeQuery();
+
+				if (resultSet.next()) {
+					cliente.setId(resultSet.getInt(1));
+					cliente.setPessoa(resultSet.getString(2));
+					cliente.setCpf_cnpj(resultSet.getString(3));
+					cliente.setPessoa(resultSet.getString(4));
+					cliente.setBloqueado(resultSet.getByte(5));
+				}
+
+				// Fechando conexões
+				preStatement.close();
+				resultSet.close();
+				conexao.close();
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		return cliente;
+	}
+
+	@Override
+	public boolean existe(String cpf_cnpj) {
+		boolean resposta;
+		if (getIndice(cpf_cnpj) >= 0)
+			resposta = true;
+		else
+			resposta = false;
+		return resposta;
 	}
 
 	private int getIndice(String cpf_cnpj) {
-	        int resposta = -1;
+		int resposta = -1;
 
-	        String sql ="select * from cliente where cpf_cnpj = ?";
-	        // Criar o PreparedStatement, objeto para executar a query
-	        PreparedStatement preStatement = conexao.prepareStatement(sql);
-	        
-	        preStatement.setInt(1, 2);
-	        // Executando o select
-	        ResultSet resultSet = preStatement.executeQuery();
-	        
-	        while(resultSet.next()){
-	        	if(resultSet.getString(3).eq){
-	        		resultSet.getString(1);
-	        	}
-	        }
-	        
-	        
-	        
-	        
-	        
-//	        for(Cliente c : clientes){
-//	            if (c.getCpf().equals(cpf)) {
-//	                resposta = c.getId();
-//	            }
-//	        }
-	        return resposta;
-	    }
-	
-	public boolean listaVazia(){
-//		if(clientes.isEmpty() == true){
-//			return true;
-//		}else{
-//			return false;
-//		}
-		return true;
+		try {
+			String sql = "select * from clientes where cpf_cnpj = ?";
+			PreparedStatement preStatement = conexao.prepareStatement(sql);
+			preStatement.setString(1, cpf_cnpj);
+			ResultSet resultSet = preStatement.executeQuery();
+
+			while (resultSet.next()) {
+				resposta = resultSet.getInt(3);
+			}
+
+			// Fechando conexões
+			preStatement.close();
+			resultSet.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return resposta;
+	}
+
+	public boolean listaVazia() {
+		boolean resposta = false;
+		try {
+			String sql = "select * from clientes";
+			PreparedStatement preStatement = conexao.prepareStatement(sql);
+			ResultSet resultSet = preStatement.executeQuery();
+
+			if (resultSet.next() == false) {
+				resposta = true;
+			}
+
+			// Fechando conexões
+			preStatement.close();
+			resultSet.close();
+			conexao.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return resposta;
 	}
 }
